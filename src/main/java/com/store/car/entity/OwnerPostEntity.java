7dc1bccb1229bdefce3fea0e31cca82182045456
Guilherme Dias <guidias200220@gmail.com>
2024-10-08 12:14:22 -0300

    feat: update OwnerPostEntity

--- src/main/java/com/store/car/entity/OwnerPostEntity.java
+++ src/main/java/com/store/car/entity/OwnerPostEntity.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "car_post")
+@Table(name = "owner_post")
 @Data
 @NoArgsConstructor
 public class OwnerPostEntity {
